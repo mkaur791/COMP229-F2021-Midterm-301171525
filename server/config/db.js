6eff5ff792aca731d29f6c4173625e807c462593
Manvir Kaur <mkaur791@my.centennialcollege.ca>
2021-10-25 21:17:21 -0400

    added mongo DB atlas URI to db config

--- server/config/db.js
+++ server/config/db.js
@@ -1,4 +1,4 @@
 module.exports = {
   //local MongoDB deployment ->
-  "URI": "mongodb://localhost/books229"
+  "URI": "mongodb+srv://admin:2Dls92nt4MnOPJQl@favouritebooklist.012ee.mongodb.net/FavouriteBookList?retryWrites=true&w=majority"
 };
