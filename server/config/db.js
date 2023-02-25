ab893a8653736aa7c4dd6ad601a1d119991fc962
Manvir Kaur <mkaur791@my.centennialcollege.ca>
2023-02-24 19:32:08 -0500

    removed console message

--- server/config/db.js
+++ server/config/db.js
@@ -3,7 +3,6 @@ Author Name: Manvir Kaur
 Student Id: 301171525
 Web app name: Favourite Book List 
 */
-console.log(process.env.DATABASE_PASSWORD)
 module.exports = {
   // MongoDB Atlas deployment ->
   "URI": `mongodb+srv://admin:${process.env.DATABASE_PASSWORD}@favouritebooklist.012ee.mongodb.net/${process.env.DATABASE_NAME}?retryWrites=true&w=majority`
